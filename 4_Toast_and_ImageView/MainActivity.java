3e33063ac5c375d2ab83106f63c228b6d7f93ee2
Moojun <mgim32137@gmail.com>
2022-09-16 16:59:19 +0900

    day4

---
+++ 4_Toast_and_ImageView/MainActivity.java
@@ -0,0 +1,52 @@
+package com.example.day4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button btn_move;
+    private EditText editText;
+    private String str;
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = findViewById(R.id.editText);
+
+        btn_move = findViewById(R.id.btn_move);
+        btn_move.setOnClickListener(new View.OnClickListener() {
+
+                @Override
+                public void onClick(View view) {
+                str = editText.getText().toString(); // 여기 선언해야, onClick event 이후 데이터가 전달됨
+
+                Intent intent = new Intent(MainActivity.this, SubActivity.class);
+                intent.putExtra("str", str);
+
+                startActivity(intent);  // activity 이동
+            }
+        });
+
+        imageView = (ImageView) findViewById(R.id.imageView);
+        imageView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "Toast Button test!!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
---
+++ 4_Toast_and_ImageView/SubActivity.java
@@ -0,0 +1,25 @@
+package com.example.day4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SubActivity extends AppCompatActivity {
+
+    private TextView textview_sub;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sub);
+
+        textview_sub = findViewById(R.id.textview_sub);
+
+        Intent intent = getIntent();
+        String str = intent.getStringExtra("str");
+
+        textview_sub.setText(str);
+    }
+}
